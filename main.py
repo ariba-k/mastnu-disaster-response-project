d0458f2a8e33f3fa18f3b954ff2d6412945da7ba
ce1estia1teapot <godincarter17@gmail.com>
2023-05-09 00:29:54 -0400

    -Created boats.py to hold the Boat class and BoatType enum
    -Created State to represent the state of a block, the StatesGrid to hold a grid of GridSquares, each of which is a State and (potentially) a Boat if present

--- main.py
+++ main.py
@@ -1,32 +1,21 @@
 """
-1. Define State Class
-1.b Define the two types of boats (Rescue and resupply)
-2. Define grid of states
-3. Determine optimal placement for resupply boat
-4. Invoke path finder to hand back the best path between every unique combination of two points of interest
-5. Instantiate Shashank's MaSTNU class. Add all nodes, and introduce edges by converting each path
-    into nodes and edges, the edges being primarily contingent links (the upper bound being the
-    path distance returned in #3, and the lower bound being the ideal distance if the agent were
-    able to travel in a straight line there)
+Problem Statement:
 
-6. There are different types of Nodes: Travel/location nodes, Rescue nodes, resupply
+There are 3 boats performing a complex rescue operation. Groups of people spread across a flooded map dotted with randomly
+placed/clustered obstacles. The people need help in a timely manner, some needing immediate medical attention, some trapped
+beneath rubble, others are in need of immediate evacuation, and some are a combination of all of these.
 
-    Travel - Happens at location, takes time to get there
-    Rescue - Happens at location, takes time to load/unload people
-    Resupply - Happens at location (initially undetermined), takes
-                time to resupply, also invoked by resource constraints
-                (Passenger capacity, fuel level, etc.)
+The goal here is to organize our 3 boats (a technical boat to clear debris, a medical boat for treatment, and a rescue boat
+for evacuation) such that all disaster victims are optimally freed, aided, and evacuated in the shortest time possible.
+This task includes interdependent tasks like clearing debris to access trapped victims in need of medical aid, etc.
 
-    Each node of a type has a linear constraint (like the location or the cargo value it happens at)
-    and a duration window [min, max] over which it could happen. For travel nodes, they happen at a
-    location (you travel to that location) and it takes you some time to get there. Resupply and
-    rescue both also happen at a location, and have some duration associated with each as well,
-    but resupply has the added constraint of being performed at particular cargo values
-
-7. Check whether MaSTNU could be decomposed to all agents; check dynamic controllability
-
-Shashank - incorporating MaSTNU code from Pset here
-Carter - Defining states, state grid, and boats
-Ariba - Incorporate path finder
 
 """
+
+from state import State, StatesGrid
+
+# ==== STEPS ====
+
+# Create boats
+
+# Create 3 StatesSims, each with increasing complexity and difficulty
