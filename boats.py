2279a28b6426dac1dc89b4c3a280dd5771b2942f
Ariba Khan <akhan02@mit.edu>
2023-05-11 18:18:23 -0400

    added activity and location code to main.py

--- boats.py
+++ boats.py
@@ -2,7 +2,6 @@ from enum import Enum
 
 
 class Boat:
-
     class BoatType(Enum):
         RESCUE = 1
         MEDICAL = 2
@@ -20,20 +19,19 @@ class Boat:
     cargoCap: int = None
     currentCargo: int = None
 
-    def __init__(self, p_boatType: BoatType=None, p_coordinates: tuple[int, int]=None, p_moveSpeed: int=1, p_cargoCap: int=2, p_currCargo: int=0):
+    def __init__(self, p_boatType: BoatType = None, p_coordinates: tuple[int, int] = None, p_moveSpeed: int = 1,
+                 p_cargoCap: int = 2, p_currCargo: int = 0):
 
         if p_boatType is None:
             raise Exception('Provided boat type cannot be None')
         if p_coordinates is None:
             raise Exception('Provided coordinates cannot be None')
 
-
         self.type = p_boatType
         self.coordinates = p_coordinates
         self.move_speed = p_moveSpeed
         self.cargoCap = p_cargoCap
         self.currentCargo = p_currCargo
-
 
     def move(self):
         raise NotImplementedError()
--- main.py
+++ main.py
@@ -15,7 +15,9 @@ import random
 
 from numpy import ndarray
 import numpy as np
+import networkx as nx
 from enum import Enum
+import matplotlib.pyplot as plt
 
 
 class Activity:
@@ -44,57 +46,164 @@ class Location:
     # Entries are of the form:  {Location#: Distance}
     distances: dict[int, float] = None
 
-    def __init__(self, p_number: int, p_activities: list[Activity]=None, p_coords: tuple[int, int]=None):
+    def __init__(self, p_number: int, p_activities: list[Activity] = None, p_coords: tuple[int, int] = None):
         self.number = p_number
-        self.activities = p_activities
+        self.activities = p_activities or []
         self.coords = p_coords
+        self.distances = {}
 
-    def fillLocationWithRandomActivities(self):
+    def fill_location_with_random_activities(self):
         """
         This method should generate a random list of activities and set that as the object's 'activities' field
         :return:
         """
+        activity_types = list(Activity.ActivityType)
+        num_activities = random.randint(1, len(activity_types))
+
+        # Randomly sample the specified number of activity types without replacement
+        selected_activity_types = random.sample(activity_types, num_activities)
+
+        # Create activities with the selected activity types and assign them to the location
+        self.activities = [Activity(activity_type) for activity_type in selected_activity_types]
+
     @staticmethod
-    def calculateDistanceBetweenPoints(p_point1: tuple[int, int], p_point2: tuple[int, int]) -> float:
+    def calculate_distance_between_locations(p_point1: tuple[int, int], p_point2: tuple[int, int]) -> float:
         # FIXME: Does this return what I think it does?
         return math.dist(p_point1, p_point2)
 
-numLocations: int = 2
-pointsList: list[tuple[int, int]] = []
-locationsList: list[Location] = []
+
+def create_graph(locations: list[Location]):
+    G = nx.DiGraph()
+    for location in locations:
+        activity_count = len(location.activities)
+        for idx, activity in enumerate(location.activities):
+            G.add_node((location.number, activity.type), activity=activity)
+
+            # Connect activities within the same location (intra-edges)
+            if idx < activity_count - 1:
+                next_activity = location.activities[idx + 1]
+                # connectionstyle="arc3,rad=0.1"
+                G.add_edge((location.number, activity.type), (location.number, next_activity.type),
+                           edge_type='intra', color='b')
+
+            for other_location in locations:
+                # Connect activities of the same type between locations in ascending order (inter-edges)
+                if other_location.number > location.number:
+                    other_activity = next((a for a in other_location.activities if a.type == activity.type), None)
+                    if other_activity:
+                        G.add_edge((location.number, activity.type), (other_location.number, other_activity.type),
+                                   edge_type='inter', color='k')
+    return G
+
+
+def draw_graph(G, dim):
+    ncols, nrows = dim
+    fig, ax = plt.subplots()
+
+    # Define the positions in a grid layout
+    pos = {(location_number, activity_type): (coords[1] + 0.5 * (activity_type.value - 2), nrows - coords[0])
+           for location_number, coords in [(loc.number, loc.coords) for loc in locations_list]
+           for activity_type in Activity.ActivityType}
+
+    # Draw the nodes in different colors based on activity type
+    color_map = {Activity.ActivityType.TECHNICAL: "skyblue", Activity.ActivityType.RESCUE: "lightgreen",
+                 Activity.ActivityType.MEDICAL: "salmon"}
+    node_colors = [color_map[node[1]] for node in G.nodes]
+
+    edge_colors = nx.get_edge_attributes(G, 'color').values()
+    # Draw the nodes with smaller size
+    nx.draw(G, pos, with_labels=False, node_size=500, node_color=node_colors, edge_color=edge_colors,
+            font_size=10, font_weight="bold")
+
+    # Create a legend for the activity names
+    legend_elements = [plt.Line2D([0], [0], marker='o', color='w', label=activity_type.name, markersize=10,
+                                  markerfacecolor=color_map[activity_type])
+                       for activity_type in Activity.ActivityType]
+
+    # Draw circles and location labels
+    for loc in locations_list:
+        # Draw a circle around activities within a location
+        max_activity_distance = max(
+            math.dist(pos[loc.number, at1], pos[loc.number, at2]) for at1 in Activity.ActivityType for at2 in
+            Activity.ActivityType) / 2
+        circle = plt.Circle((loc.coords[1], nrows - loc.coords[0]), max_activity_distance, fill=False,
+                            color=f'C{loc.number % 10}', linestyle='dashed', linewidth=1)
+        ax.add_artist(circle)
+
+        # Add a legend for the location numbers
+        legend_elements.append(
+            plt.Line2D([0], [0], marker='o', color='w', linestyle='None', label=f'Location {loc.number}', markersize=10,
+                       markerfacecolor=f'C{loc.number % 10}', markeredgewidth=0))
+
+    plt.legend(handles=legend_elements, loc='best', title='Activity Types & Locations')
+
+    plt.show()
+
+
+num_locations: int = 2
+points_list: list[tuple[int, int]] = []
+locations_list: list[Location] = []
 
 # ==== STEPS TO LOOP FOR EACH SIMULATION ====
 # Create nxn grid
-rows = 10*numLocations
+rows = 10 * num_locations
 cols = rows
 
 # Create Locations at random points
-for i in range(1, numLocations):
+for i in range(1, num_locations + 1):
     # Generates random points until one is made that isn't already in the list of points
-    randPoint: tuple[int, int]
+    rand_point: tuple[int, int]
     while True:
-        randRow: int = random.randrange(0, rows)
-        randCol: int = random.randrange(0, rows)
-        randPoint = (randRow, randCol)
+        rand_row: int = random.randrange(0, rows)
+        rand_col: int = random.randrange(0, rows)
+        rand_point = (rand_row, rand_col)
 
-        if not (randPoint in pointsList):
-            pointsList.append(randPoint)
+        if not (rand_point in points_list):
+            points_list.append(rand_point)
             break
 
-    tempLocation: Location = Location(p_number=i, p_coords=randPoint)
-    tempLocation.fillLocationWithRandomActivities()
-    locationsList.append(tempLocation)
+    temp_location: Location = Location(p_number=i, p_coords=rand_point)
+    temp_location.fill_location_with_random_activities()
+    locations_list.append(temp_location)
+
+for loc1 in locations_list:
+    for loc2 in locations_list:
+        if loc1.number != loc2.number:
+            loc1.distances[loc2.number] = Location.calculate_distance_between_locations(loc1.coords, loc2.coords)
 
 
+def print_edges(G):
+    intra_edges = []
+    inter_edges = []
 
+    for edge in G.edges(data=True):
+        if edge[2]['edge_type'] == 'intra':
+            intra_edges.append(edge)
+        elif edge[2]['edge_type'] == 'inter':
+            inter_edges.append(edge)
+
+    print("Intra-location edges:")
+    for edge in intra_edges:
+        print(edge)
+
+    print("\nInter-location edges:")
+    for edge in inter_edges:
+        print(edge)
+
+
+G = create_graph(locations_list)
+print(G)
+print(G.nodes)
+print_edges(G)
+draw_graph(G, (cols, rows))
 
 # Calculate distances between all pairs of locations, store on each location
 
 # Use distances to calculate time bounds [min, max] between locations
 
-# For each location, generate the activities for the location. A random amount in some range dictated by the "difficulty" of
-# this particular test. At most one of each type:
-#     Randomly determine (within a range) the durations of each activity of each type
+# For each location, generate the activities for the location. A random amount in some range dictated by the
+# "difficulty" of this particular test. At most one of each type: Randomly determine (within a range) the durations
+# of each activity of each type
 
 # Link together all the activities at the location (determining the order in which they must happen)
 
